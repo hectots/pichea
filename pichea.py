cb871c3c2ff6876f63b9b5c17cb9b0b76c50be0f
Hector Toledo Soto <hsoto@transperfect.com>
2021-08-25 17:17:43 -0400

    Can save request now.

--- pichea.py
+++ pichea.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
 
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import argparse
 import os
-import sys
+
 
 class MockRequestHandler(BaseHTTPRequestHandler):
     protocol_version = "HTTP/1.1"
 
     def processRequest(self, method):
-        (base_path, _) = self.path.split("?") if "?" in self.path else (self.path, "")
+        (base_path, _) = self.path.split(
+            "?") if "?" in self.path else (self.path, "")
         directory = os.path.dirname(base_path)
+
+        if self.save_request:
+            requestFileName = f'{method}_{os.path.basename(base_path)}.request.txt'
+            requestFileFullPath = os.path.join(
+                self.mock_path,
+                directory[1:],  # Deletes starting '/'
+                requestFileName)
+
+            with open(requestFileFullPath, 'wb') as request_file:
+                request_file.write(self.rfile.read(
+                    int(self.headers["Content-Length"])))
+
         filename = f'{method}_{os.path.basename(base_path)}.json'
         fullpath = os.path.join(
             self.mock_path,
-            directory[1:], # Deletes starting '/'
+            directory[1:],  # Deletes starting '/'
             filename)
 
         body = ""
@@ -30,16 +44,28 @@ class MockRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.processRequest("GET")
-    
+
     def do_POST(self):
         self.processRequest("POST")
-    
+
     def do_PUT(self):
         self.processRequest("PUT")
-    
+
     def do_DELETE(self):
         self.processRequest("DELETE")
 
-s = ThreadingHTTPServer(('localhost', 8282), type("MockRequestHandlerAug", (MockRequestHandler,), dict(mock_path=sys.argv[1] if len(sys.argv[1:]) > 0 else ".")))
-s.serve_forever()
-s.server_close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "pichea", description="Maps HTTP requests to JSON files.")
+    parser.add_argument("--save-request", "-s",
+                        action='store_true', help="Saves requests to a file.")
+    parser.add_argument(
+        "mock_path", help="Base path to JSON files. Default is the current directory.")
+
+    args = parser.parse_args()
+
+    s = ThreadingHTTPServer(('localhost', 8282), type("MockRequestHandlerAug", (MockRequestHandler,), dict(
+        mock_path=args.mock_path, save_request=args.save_request)))
+    s.serve_forever()
+    s.server_close()
